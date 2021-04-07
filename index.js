7edb09434cdee2f4dd075fea149624926f2d3521
unknown <mairotg@gmail.com>
2021-04-07 10:41:30 +0200

    fix

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const swaggerJsDoc = require("swagger-jsdoc");
 require('dotenv').config();
 const promosRouter = require("./routes/promos");
 
-//const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 4000;
 
 const options = {
 	definition: {
@@ -19,7 +19,7 @@ const options = {
 		servers: [
 			{
 				//url: "http://localhost:4000",
-				url: "https://mspr-epsi.tomco.tech",
+				url: "https://mspr-epsi.tomco.tech:4000",
 			},
 		],
 	},
