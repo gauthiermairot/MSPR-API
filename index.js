a0aa44f77f141862499178cbacd2ad154d0c1f86
unknown <mairotg@gmail.com>
2021-04-07 11:56:15 +0200

    fix

--- index.js
+++ index.js
@@ -8,31 +8,31 @@ const promosRouter = require("./routes/promos");
 
 // const PORT = process.env.PORT || 80;
 
-// const options = {
-// 	definition: {
-// 		openapi: "3.0.0",
-// 		info: {
-// 			title: "Library API",
-// 			version: "1.0.0",
-// 			description: "A simple Express Library API",
-// 		},
-// 		servers: [
-// 			{
-// 				//url: "http://localhost",
-// 				url: "https://mspr-epsi.tomco.tech",
-// 			},
-// 		],
-// 	},
-// 	apis: ["./routes/*.js"],
-// };
+const options = {
+	definition: {
+		openapi: "3.0.0",
+		info: {
+			title: "Library API",
+			version: "1.0.0",
+			description: "A simple Express Library API",
+		},
+		servers: [
+			{
+				//url: "http://localhost",
+				url: "https://mspr-epsi.tomco.tech",
+			},
+		],
+	},
+	apis: ["./routes/*.js"],
+};
 
-// const specs = swaggerJsDoc(options);
-// const app = express();
-// app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
-// app.use(cors());
-// app.use(express.json());
-// app.use(morgan("dev"));
-// app.use("/promos", promosRouter);
+const specs = swaggerJsDoc(options);
+const app = express();
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+app.use("/promos", promosRouter);
 // app.listen('/');
 
 const app = express()
