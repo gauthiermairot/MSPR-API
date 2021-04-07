16441c01316b2171000a90c2df7d841293b3950a
unknown <mairotg@gmail.com>
2021-04-07 12:00:35 +0200

    fix

--- index.js
+++ index.js
@@ -40,6 +40,17 @@ app.get('/a', (req, res) => {
   res.send('Hello blog reader!')
 })
 
+let con = mysql.createConnection({
+	host: process.env.MYSQL_HOST,
+	user: process.env.MYSQL_USER,
+	password: process.env.MYSQL_PWD,
+	database: process.env.MYSQL_DB,
+});
+
+con.connect(function(error) {
+	if (error) console.log(error);
+	else console.log("connected");
+});
 app.get("/test", (req, res) => {
 	con.query(`select * from QR_CODE`, function(error, rows, fields) {     
 		res.send(rows);
